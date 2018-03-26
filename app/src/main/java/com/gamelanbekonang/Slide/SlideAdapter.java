d0f7d6f87a6a54732f7e8912d5a9ae32a040d6e5
Unknown <abdulmalik1878@gmail.com>
2018-03-26 23:54:19 +0700

    tambah gambar

---
+++ app/src/main/java/com/gamelanbekonang/Slide/SlideAdapter.java
@@ -0,0 +1,48 @@
+package com.gamelanbekonang.Slide;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by root on 06/03/18.
+ */
+
+public class SlideAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+    private int []layouts;
+
+    public SlideAdapter(int[] layouts, Context context){
+        this.context = context;
+        this.layouts = layouts;
+    }
+
+    @Override
+    public int getCount() {
+        return layouts.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(layouts[position], container, false);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        View view = (View)object;
+        container.removeView(view);
+    }
+}
---
+++ app/src/main/java/com/gamelanbekonang/Splash/SplashScreen.java
@@ -0,0 +1,40 @@
+package com.gamelanbekonang.Splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.gamelanbekonang.HomeActivity;
+import com.gamelanbekonang.R;
+
+public class SplashScreen extends AppCompatActivity {
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        iv = findViewById(R.id.iv);
+        Animation myanim = new AnimationUtils().loadAnimation(this,R.anim.mytransitiont);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(this, HomeActivity.class);
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000) ;
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+
+            }
+        };
+        timer.start();
+    }
+}
