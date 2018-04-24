c8f493c26e8eb57474eada2e9aa1519b42720830
Dio_Satriani <diosatriani44@gmail.com>
2018-04-24 22:04:01 +0700

    logRes dan menu akun

---
+++ app/src/main/java/com/gamelanbekonang/menuAkun/TentangActivity.java
@@ -0,0 +1,36 @@
+package com.gamelanbekonang.menuAkun;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.gamelanbekonang.R;
+
+public class TentangActivity extends AppCompatActivity {
+
+    private Toolbar mActionToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang);
+
+        mActionToolbar = (Toolbar) findViewById(R.id.tabs);
+        setSupportActionBar(mActionToolbar);
+        getSupportActionBar().setTitle("TENTANG");
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+    }
+
+    //button back toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()== android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+}
