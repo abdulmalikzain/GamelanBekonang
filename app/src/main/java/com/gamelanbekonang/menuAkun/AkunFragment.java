84f91f1166292203b51137913e4327177428b0eb
Unknown <abdulmalik1878@gmail.com>
2018-04-26 10:28:24 +0700

    push baru

--- app/src/main/java/com/gamelanbekonang/menuAkun/AkunFragment.java
+++ app/src/main/java/com/gamelanbekonang/menuAkun/AkunFragment.java
@@ -4,20 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.gamelanbekonang.adapter.AdapterAkun;
 import com.gamelanbekonang.logRes.LoginActivity;
 import com.gamelanbekonang.R;
+
+import java.util.ArrayList;
 
 
 public class AkunFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> InfoAkun;
+    private ArrayList<Integer> GambarAkun;
+    //nama listnya
+    private String[] Nama = {"TENTANG", "BANTUAN"};
+    //Daftar gambar
+    private int[] Gambar = {R.drawable.ic_akun, R.drawable.ic_home};
 
     Button btnlogin;
-
     public AkunFragment() {
         // Required empty public constructor
 
@@ -38,6 +52,19 @@ public class AkunFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        InfoAkun = new ArrayList<>();
+        GambarAkun = new ArrayList<>();
+        recyclerView = view.findViewById(R.id.rv_akun);
+        DaftarItem();
+
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        adapter = new AdapterAkun(InfoAkun, GambarAkun);
+        //Memasang Adapter pada RecyclerView
+        recyclerView.setAdapter(adapter);
+
         btnlogin = (Button) view.findViewById(R.id.btn_masuk);
 
         btnlogin.setOnClickListener(new View.OnClickListener() {
@@ -48,5 +75,12 @@ public class AkunFragment extends Fragment {
             }
         });
 
+    }
+
+    private void DaftarItem() {
+        for (int w=0; w<Nama.length; w++){
+            GambarAkun.add(Gambar[w]);
+            InfoAkun.add(Nama[w]);
+        }
     }
 }
