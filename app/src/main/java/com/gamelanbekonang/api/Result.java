df88212ed2de64de6889f9ee3547f83d21bb4eb9
Unknown <diosatriani44@gmail.com>
2018-05-23 17:30:20 +0700

    api_result

---
+++ app/src/main/java/com/gamelanbekonang/api/Result.java
@@ -0,0 +1,18 @@
+package com.gamelanbekonang.api;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Result {
+    @SerializedName("result")
+    @Expose
+    private String result;
+
+    public String getResult(){
+        return  result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
